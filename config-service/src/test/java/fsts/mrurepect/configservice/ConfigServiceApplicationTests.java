a96da2cc655e25e4baf4dcecb89186b2c4bbee38
EL FADILI MOHAMED <580.simo@gmail.com>
2024-04-18 00:50:39 +0100

    adding config service

---
+++ config-service/src/test/java/fsts/mrurepect/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fsts.mrurepect.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
